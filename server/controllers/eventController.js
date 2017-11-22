6f33a01dd1b01d13a1751f7345896f6d67e90f80
leksyib <leksyib14@gmail.com>
2017-11-22 12:28:53 +0100

    ft(set up routes and controller to get all events)

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -1,25 +1,5 @@
 import express from 'express';
-//import { events } from '../data/eventsData'
-const events = [
-  {
-    id: 1,
-    name: "GDG DEVFEST",
-    description: "Traning worldclass developers",
-    date: 10-23-2017
-  },
-  {
-    id: 2,
-    name: "Python Conference",
-    description: "Traning worldclass developers",
-    date: 10-23-2017
-  },
-  {
-    id: 3,
-    name: 'Forloop Abuja',
-    description: 'Traning worldclass developers',
-    date: 10-23-2017
-},
-];
+import { events } from '../models/eventsData'
 
 exports.getAllEvents = (req, res) => {
     res.json({
--- server/models/eventsData.js
+++ server/models/eventsData.js
@@ -1,15 +1,18 @@
 const events = [
   {
+    id: 1,
     name: "GDG DEVFEST",
     description: "Traning worldclass developers",
     date: 10-23-2017
   },
   {
+    id: 2,
     name: "Python Conference",
     description: "Traning worldclass developers",
     date: 10-23-2017
   },
   {
+    id: 3,
     name: 'Forloop Abuja',
     description: 'Traning worldclass developers',
     date: 10-23-2017

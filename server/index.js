0d5de6f62d74c21492e0d2504d8a2bd28b6250db
leksyib <leksyib14@gmail.com>
2017-11-24 10:40:02 +0100

    Ignore irrelevant files

--- server/index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-// by requiring `babel/register`, all of our successive `require`s will be Babel'd
-require("babel-register")({
-  presets: ["es2015"]
-});
-
-require("./app");
--- template/assets/js/init.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-  $(function() {
-    $(".button-collapse").sideNav();
-    $(".parallax").parallax();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space

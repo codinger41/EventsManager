31aff1bc595499b1d02d8a4745e5636d3ec89078
leksyib <leksyib14@gmail.com>
2017-11-26 11:03:16 +0100

    Add after_success script to .travis.yml

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // to parse req body content
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   console.log('eventsmanager!');
